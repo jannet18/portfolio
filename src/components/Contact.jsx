c1814a0aebd92b3143c80ddb4a8a45f091c12854
jannet18 <janetngei18@gmail.com>
2024-10-01 19:26:15 +0300

    cntered footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,18 +32,18 @@ const Contact = () => {
   const notify = () => toast("Success");
   return (
     <div className="bg-black text-white py-20" id="contact">
-      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+      <div className="container mx-auto px-10 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">Contact Me</h2>
-        <div className="flex flex-col md:flex-row items-center md:space-x-12">
-          <div className="flex-1">
+        <div className="flex flex-col md:flex-row items-center justify-center space-y-3 md:space-x-12">
+          <div className="flex-1 tracking-wider text-center">
             <h3
               className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r 
             from-pink-500 to-yellow-500 mb-4"
             >
               Let's Talk
             </h3>
-            <p>
-              I'm open to discussing web development prjectgs or partnership
+            <p className="">
+              I'm open to discussing web development projects or partnership
               opportunities.
             </p>
             <div className="mb-4 mt-8">
@@ -64,8 +64,8 @@ const Contact = () => {
               <span>Nairobi, Kenya.</span>
             </div>
           </div>
-          <div className="flex-1 w-full">
-            <form className="space-y-4" ref={form} onSubmit={sendEmail}>
+          <div className="flex-1 w-full p-3">
+            <form className="space-y-8 " ref={form} onSubmit={sendEmail}>
               <div>
                 <label htmlFor="name" className="block mb-2">
                   Name
@@ -103,15 +103,18 @@ const Contact = () => {
                   name="message"
                 />
               </div>
-              <button
-                type="submit"
-                onClick={notify}
-                // value={send}
-                className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white md:inline
+              <div className="text-center">
+                <button
+                  type="submit"
+                  onClick={notify}
+                  // value={send}
+                  className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white md:inline
             transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-full"
-              >
-                Send
-              </button>
+                >
+                  Send
+                </button>
+              </div>
+
               <ToastContainer />
             </form>
           </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -66,7 +66,7 @@ const Projects = () => {
     <div className="bg-black text-white py-20" id="project">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">My Projects</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {projects.map((project) => (
             <div
               key={project.id}
@@ -81,7 +81,9 @@ const Projects = () => {
               />
               <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
               <p className="text-gray-400 mb-4">{project.technologies}</p>
-              <p className="text-gray-400 mb-4">{project.description}</p>
+              <p className="text-gray-400 mb-4 tracking-wider">
+                {project.description}
+              </p>
               <div className="space-x-4">
                 <a
                   href={project.github}
